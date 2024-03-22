66016f272c07b34a235f9d136421c8f3cc651e69
adoerr <0xad@gmx.net>
2024-03-22 15:16:38 +0100

    Hello World from Rust
    Signed-off-by: adoerr <0xad@gmx.net>

---
+++ hello-world/src/lib.rs
@@ -0,0 +1,14 @@
+#![no_std]
+
+extern "C" {
+    pub fn printk(text: *const u8);
+}
+
+use panic_halt as _;
+
+#[no_mangle]
+pub extern "C" fn rust_main() {
+    unsafe {
+        printk("\tHello World from Rust\n\n\0".as_ptr());
+    }
+}
---
+++ hello-world/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+
+/* External declaration for main in Rust. */
+void rust_main(void);
+
+int main(void)
+{
+	printk("Starting Rust\n\n");
+	rust_main();
+	printk("Done with Rust");
+}
