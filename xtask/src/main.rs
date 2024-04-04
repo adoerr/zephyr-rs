0b8aeb1d5b77719938b9ceb3dad4a0e178c8c596
adoerr <0xad@gmx.net>
2024-04-04 15:57:29 +0200

    Add bindgen subcommand
    Signed-off-by: adoerr <0xad@gmx.net>

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,33 @@
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+/// xtask
+struct CLI {
+    #[argh(subcommand)]
+    command: Command,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+enum Command {
+    Bindgen(Bindgen),
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Generate Rust bindings from Zephyr headers
+#[argh(subcommand, name = "bindgen")]
+struct Bindgen {
+    #[argh(option)]
+    /// header file with Zephyr definitions
+    header: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli: CLI = argh::from_env();
+
+    match cli.command {
+        Command::Bindgen(bindgen) => {
+            println!("bindgen: {:?}", bindgen);
+        }
+    }
 }
