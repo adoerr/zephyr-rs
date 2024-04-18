d488bfcdba8f96792e78c1973a6bb3d9d4140e54
adoerr <0xad@gmx.net>
2024-04-18 10:17:35 +0200

    Global allocator
    Signed-off-by: adoerr <0xad@gmx.net>

--- crates/zephyr-alloc/src/lib.rs
+++ crates/zephyr-alloc/src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use core::alloc::{GlobalAlloc, Layout};
+
+pub use sys::k_heap;
+use zephyr_sys as sys;
+
+pub struct Mempool(pub &'static k_heap);
+
+unsafe impl Send for Mempool {}
+unsafe impl Sync for Mempool {}
+
+unsafe impl GlobalAlloc for Mempool {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let kheap = self.0 as *const _ as *mut _;
+
+        let ret = sys::k_heap_alloc(kheap, layout.size(), sys::K_NO_WAIT) as *mut _;
+
+        if ret as usize & (layout.align() - 1) != 0 {
+            sys::printk("Rust unsatisfied alloc alignment\n\0".as_ptr() as *const core::ffi::c_char);
+            core::ptr::null_mut()
+        } else {
+            ret
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        let kheap = self.0 as *const _ as *mut _;
+        sys::k_heap_free(kheap, ptr as *mut _)
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Assign a Zephyr k_heap as #[global_allocator]
+///
+/// This should be defined with K_HEAP_DEFINE and granted permission to any
+/// Rust threads that need to use alloc.
+#[macro_export]
+macro_rules! global_mem_pool {
+    ($pool:ident) => {
+        extern "C" {
+            static $pool: $crate::mempool::k_heap;
+        }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+        #[global_allocator]
+        static GLOBAL: $crate::mempool::Mempool = $crate::mempool::Mempool(unsafe { &$pool });
+    };
 }
