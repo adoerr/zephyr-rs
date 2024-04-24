21a94e409a024e11004564b8a594641d27d58f13
adoerr <0xad@gmx.net>
2024-04-24 09:32:50 +0200

    Add crate path argument
    Signed-off-by: adoerr <0xad@gmx.net>

--- tools/zephyr-bindgen/src/main.rs
+++ tools/zephyr-bindgen/src/main.rs
@@ -1,21 +1,35 @@
-use std::{env, path::PathBuf};
+use std::{env, fs, path::PathBuf};
 
 use anyhow::Result;
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+#[allow(clippy::upper_case_acronyms)]
+/// xtask
+struct CLI {
+    #[argh(positional)]
+    /// path to crate for which to generate bindings
+    crate_path: PathBuf,
+}
 
 fn main() -> Result<()> {
+    let cli: CLI = argh::from_env();
+
+    let crate_path = fs::canonicalize(cli.crate_path)?;
+    eprintln!("zephyr-bindgen: crate path `{}`", crate_path.display());
+
     let flags = env::var("TARGET_CFLAGS").unwrap_or("".to_string());
-    eprintln!("cflags: {}", flags);
+    eprintln!("zephyr-bindgen: cflags `{}`", flags);
 
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header("wrapper.h")
+        .header(crate_path.join("wrapper.h").to_str().unwrap())
         .use_core()
         .clang_args(flags.split(' '))
         .generate()?;
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_path.join("bindings.rs"))?;
+    bindings.write_to_file(crate_path.join("src/bindings.rs"))?;
 
     Ok(())
 }
