69fcadd0819c72ccb0e3bf6b5588eb48bf0d97af
adoerr <0xad@gmx.net>
2024-04-24 15:16:10 +0200

    Fetch Zephyr build config
    Signed-off-by: adoerr <0xad@gmx.net>

--- tools/zephyr-bindgen/src/main.rs
+++ tools/zephyr-bindgen/src/main.rs
@@ -1,15 +1,19 @@
-use std::{env, fs, path::PathBuf};
+use std::{fs, path::PathBuf};
 
 use anyhow::Result;
 use argh::FromArgs;
 
 #[derive(FromArgs)]
 #[allow(clippy::upper_case_acronyms)]
-/// xtask
+/// zephyr-bindgen: generate Rust bindings for Zephyr
 struct CLI {
     #[argh(positional)]
     /// path to crate for which to generate bindings
     crate_path: PathBuf,
+
+    #[argh(positional, greedy)]
+    /// cflags to pass to clang
+    cflags: Vec<String>,
 }
 
 fn main() -> Result<()> {
@@ -18,15 +22,13 @@ fn main() -> Result<()> {
     let crate_path = fs::canonicalize(cli.crate_path)?;
     eprintln!("zephyr-bindgen: crate path `{}`", crate_path.display());
 
-    let flags = env::var("TARGET_CFLAGS").unwrap_or("".to_string());
-    eprintln!("zephyr-bindgen: cflags `{}`", flags);
+    let cflags_str = cli.cflags.join(" ");
 
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
         .header(crate_path.join("wrapper.h").to_str().unwrap())
+        .allowlist_item("k_*")
         .use_core()
-        .clang_args(flags.split(' '))
+        .clang_args(cflags_str.split_whitespace())
         .generate()?;
 
     bindings.write_to_file(crate_path.join("src/bindings.rs"))?;
